e1631ad718c792c9d3d085242184ceacc90e25db
Antonio Olvera <antonioolvera1995@gmail.com>
2021-06-12 18:20:47 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net_core_regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
